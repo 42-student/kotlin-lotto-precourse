7d91782fc16cbff69524616e3d61d1ecf2694519
Sebastian Marginean <sebastian.42student@gmail.com>
2025-04-24 23:05:29 +0200

    feat(Lotto): validate number uniqueness and range, add corresponding tests

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,8 @@ package lotto
 class Lotto(private val _numbers: List<Int>) {
     init {
         require(_numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+		require(_numbers.distinct().size == 6) { "[ERROR] Lotto numbers must be unique." }
+		require(_numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be in range 1 and 45." }
     }
 
     val numbers: List<Int> get() = _numbers
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.assertj.core.api.Assertions.assertThat
 
 class LottoTest {
     @Test
@@ -11,7 +12,6 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement production code to pass the test
     @Test
     fun `throws an exception when lotto numbers contain duplicates`() {
         assertThrows<IllegalArgumentException> {
@@ -19,5 +19,23 @@ class LottoTest {
         }
     }
 
-    // TODO: Implement tests based on the added features
+    @Test
+	fun `throws an exception when lotto numbers contain a value less than 1`() {
+		assertThrows<IllegalArgumentException> {
+			Lotto(listOf(0, 1, 2, 3, 4, 5))
+		}
+	}
+
+	@Test
+	fun `throws an exception when lotto numbers contain a value greater than 24`() {
+		assertThrows<IllegalArgumentException> {
+			Lotto(listOf(1, 2, 3, 4, 5, 46))
+		}
+	}
+
+	@Test
+	fun `creates a valid Lotto object with 6 numbers within range and no duplicates`() {
+		val lotto = Lotto(listOf(2, 4, 12, 21, 36, 42))
+		assertThat(lotto.numbers).containsExactlyInAnyOrder(2, 4, 12, 21, 36, 42)
+	}
 }
