7617f925bd53b63f401a6021ff055e659557acf1
Sebastian Marginean <sebastian.42student@gmail.com>
2025-04-26 09:21:40 +0200

    docs(readme) marked implementation as finished

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -7,7 +7,7 @@ class LottoMachine {
 		val ticketCount = amount / LottoConstants.TICKET_PRICE
 		val tickets = mutableListOf<Lotto>()
 		for (i in 1..ticketCount) {
-			val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+			val numbers = Randoms.pickUniqueNumbersInRange(LottoConstants.MIN_NUMBER, LottoConstants.MAX_NUMBER, LottoConstants.NUMBER_COUNT)
 			val ticket = Lotto(numbers)
 			tickets.add(ticket)
 		}
