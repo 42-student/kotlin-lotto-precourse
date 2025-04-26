3ef001c4cba5c4a44ed9f0e39dd33512b4931a02
Sebastian Marginean <sebastian.42student@gmail.com>
2025-04-26 07:56:50 +0200

    refactor: apply lotto constants, sort ticket numbers, shorten functions

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -2,9 +2,9 @@ package lotto
 
 class Lotto(private val _numbers: List<Int>) {
     init {
-        require(_numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
-		require(_numbers.distinct().size == 6) { "[ERROR] Lotto numbers must be unique." }
-		require(_numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be in range 1 and 45." }
+        require(_numbers.size == LottoConstants.NUMBER_COUNT) { "[ERROR] Lotto must contain exactly 6 numbers." }
+		require(_numbers.distinct().size == LottoConstants.NUMBER_COUNT) { "[ERROR] Lotto numbers must be unique." }
+		require(_numbers.all { it in LottoConstants.MIN_NUMBER..LottoConstants.MAX_NUMBER }) { "[ERROR] Lotto numbers must be in range 1 and 45." }
     }
 
     val numbers: List<Int> get() = _numbers
--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class LottoMachine {
 	fun buyTickets(amount: Int): List<Lotto> {
-		val ticketCount = amount / 1000
+		val ticketCount = amount / LottoConstants.TICKET_PRICE
 		val tickets = mutableListOf<Lotto>()
 		for (i in 1..ticketCount) {
 			val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
